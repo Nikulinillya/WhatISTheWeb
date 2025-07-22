db8a76ee741b01d0a4023db32f560872781f8e7e
Nikulinillya <nikulinilly0508@gmail.com>
2025-07-22 20:52:52 +0300

    Delete web directory

--- web/script.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener("load", () => {
-    document.body.classList.add("no-scroll");
-
-    setTimeout(() => {
-      document.body.classList.remove("no-scroll");
-    }, 3000);
-  });
--- web/slider-mob.js
+++
@@ -1,9 +0,0 @@
-const slider = document.getElementById("slider");
-
-function scrollSlider(direction) {
-  const slideWidth = slider.querySelector('.card').offsetWidth + 35;
-  slider.scrollBy({
-    left: direction * slideWidth,
-    behavior: "smooth"
-  });
-}
